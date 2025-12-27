c2e2f8175d5a4247db12130ee85227aba8c9e3f1
muhfahmm <148422221+muhfahmm@users.noreply.github.com>
2025-12-27 15:12:51 +0700

    ibox

--- admin/products-panel/ipad/api/api-edit-ipad.php
+++ admin/products-panel/ipad/api/api-edit-ipad.php
@@ -1,4 +1,4 @@
-<!-- <?php
+<?php
 session_start();
 require_once '../../../db.php';
 
@@ -170,4 +170,4 @@ try {
 
 header('Content-Type: application/json');
 echo json_encode($response);
-?> -->
+?>
